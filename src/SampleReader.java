85e7967ea0555808571a5b74dc64ced26d1975d3
Thomas Lisankie <link285@gmail.com>
2015-09-23 13:51:41 -0400

    -

--- src/SampleReader.java
+++ src/SampleReader.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class SampleReader {
 	
 	public static void main(String[] args){
-		
+		ArrayList<Phoneme> phonemes = null;
 		//1
 		List<String> linesOfDictionary = null;
 	//loads all the lines in the CMU Phonemic Dictionary. Each line contains a word and its phonemic translation.
@@ -32,12 +32,12 @@ public class SampleReader {
 		
 		for (int i = 0; i < linesOfDictionary.size(); i++){
 			
-			String wordBeingExamined = linesOfDictionary.get(i);
+			String lineBeingExamined = linesOfDictionary.get(i);
 			int indexOfCharBeingExamined = 0;
-			for (int j = 0; j < wordBeingExamined.length(); j++){
+			for (int j = 0; j < lineBeingExamined.length(); j++){
 				
 				indexOfCharBeingExamined = j;
-				char charBeingExamined = wordBeingExamined.charAt(j);
+				char charBeingExamined = lineBeingExamined.charAt(j);
 				if(charBeingExamined != ' '){
 					
 					word = word + charBeingExamined;
@@ -52,15 +52,16 @@ public class SampleReader {
 			}
 			
 			indexOfCharBeingExamined = indexOfCharBeingExamined + 2;
-			ArrayList<Phoneme> phonemes = new ArrayList<Phoneme>();
-			for (int k = indexOfCharBeingExamined; k < wordBeingExamined.length(); k++){
+			phonemes = new ArrayList<Phoneme>();
+			for (int k = indexOfCharBeingExamined; k < lineBeingExamined.length(); k++){
 				
 				Phoneme phoneme = new Phoneme();
 				String phonemeName = "";
-				char charBeingExamined = wordBeingExamined.charAt(k);
+				char charBeingExamined = lineBeingExamined.charAt(k);
 				if(charBeingExamined != ' ' || Character.isDigit(charBeingExamined) == false){ 
 					
 					phonemeName = phonemeName + charBeingExamined;
+					System.out.println(phonemeName);
 					
 				}else if(Character.isDigit(charBeingExamined)){
 					
@@ -80,6 +81,21 @@ public class SampleReader {
 			}
 			
 			listsOfPhonemesForWords.add(phonemes);
+			
+		}
+		
+		
+		for(int h = 0; h < wordNames.size(); h++){
+			
+			System.out.println(wordNames.get(h));
+			ArrayList<Phoneme> listSample = listsOfPhonemesForWords.get(h);
+			System.out.println("porehig");
+			System.out.println(listsOfPhonemesForWords);
+			for(int l = 0; l < listSample.size(); l++){
+				
+				System.out.println(listSample.get(l).getStress());
+				
+			}
 			
 		}
 
