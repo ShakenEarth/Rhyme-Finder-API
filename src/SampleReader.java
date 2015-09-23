5ecd3402ea332f9a5401488ce8658178f72153b9
Thomas Lisankie <link285@gmail.com>
2015-09-22 23:48:39 -0400

    -

--- src/SampleReader.java
+++ src/SampleReader.java
@@ -7,7 +7,8 @@ public class SampleReader {
 	
 	public static void main(String[] args){
 		
-		List<String> linesOfDictionary;
+		//1
+		List<String> linesOfDictionary = null;
 	//loads all the lines in the CMU Phonemic Dictionary. Each line contains a word and its phonemic translation.
 		try{
 			linesOfDictionary = Files.readAllLines(Paths.get("cmudictOneLiner.txt"), Charset.defaultCharset());
@@ -21,44 +22,66 @@ public class SampleReader {
 		
 		}
 		
+		//2
 		//ArrayList for the word names
 		ArrayList<String> wordNames = new ArrayList<String>();
 		
 		/**ArrayList of lists of phonemes that compose a word*/
 		ArrayList<ArrayList<Phoneme>> listsOfPhonemesForWords = new ArrayList<ArrayList<Phoneme>>();
 		String word = "";
+		
 		for (int i = 0; i < linesOfDictionary.size(); i++){
 			
-		    String wordBeingExamined = linesOfDictionary.get(i);
-		    int indexOfCharBeingExamined = 0;
-		    for (int j = 0; j < wordBeingExamined.length(); j++){
-		        indexOfCharBeingExamined = j;
-		        char charBeingExamined = wordBeingExamined[i];
-		        if(charBeingExamined != ' '){
-		            word = word + charBeingExamined;
-		    } else{
-		            wordNames.add(word);
-		            break;
-		    }
+			String wordBeingExamined = linesOfDictionary.get(i);
+			int indexOfCharBeingExamined = 0;
+			for (int j = 0; j < wordBeingExamined.length(); j++){
+				
+				indexOfCharBeingExamined = j;
+				char charBeingExamined = wordBeingExamined.charAt(j);
+				if(charBeingExamined != ' '){
+					
+					word = word + charBeingExamined;
+					
+				}else{
+					
+					wordNames.add(word);
+					break;
+					
+				}
+				
+			}
+			
+			indexOfCharBeingExamined = indexOfCharBeingExamined + 2;
+			ArrayList<Phoneme> phonemes = new ArrayList<Phoneme>();
+			for (int k = indexOfCharBeingExamined; k < wordBeingExamined.length(); k++){
+				
+				Phoneme phoneme = new Phoneme();
+				String phonemeName = "";
+				char charBeingExamined = wordBeingExamined.charAt(k);
+				if(charBeingExamined != ' ' || charBeingExamined != anInteger){ 
+				//"anInteger" and "charBeingExaminedAsAnInteger" are placeholders for now
+					
+					phonemeName = phonemeName + charBeingExamined;
+					
+				}else if(charBeingExamined == anInteger){
+					
+					phoneme.setStress(charBeingExaminedAsAnInteger);
+					
+				}else if(charBeingExamined == ' '){
+					
+					phoneme.setPhoneme(phonemeName);
+					phonemes.add(phoneme);
+					phoneme = new Phoneme();
+					phonemeName = "";
+					
+					
+				}
+				
+			}
+			
+			listsOfPhonemesForWords.add(phonemes);
+			
 		}
-	}
-		    indexOfCharBeingExamined = indexOfCharBeingExamined + 2;
-		    ArrayList<Phoneme> phonemes = new ArrayList<Phoneme>();
-		    for (int k = indexOfCharBeingExamined; k < wordBeingExamined.length; k++)
-		        Phoneme phoneme = new Phoneme();
-		        String phonemeName = “”;
-		        char charBeingExamined = wordBeingExamined[k];
-		        if(charBeingExamined != ‘ ‘ || charBeingExamined != anInteger)
-		            phonemeName = phonemeName + charBeingExamined;
-		        else if(charBeingExamined == anInteger)
-		            phoneme.setStress(charBeingExaminedAsAnInteger);
-		        else if(charBeingExamined == '')
-		            phoneme.setPhoneme(phonemeName);
-		            phonemes.add(phoneme);
-		            phoneme = new Phoneme();
-		            phonemeName = "";
-		    listsOfPhonemesForWords.add(phonemes);
-		
 
 	}
 }
