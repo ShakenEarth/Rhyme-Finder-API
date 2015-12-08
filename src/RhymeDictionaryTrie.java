4e0bce083db6b0adb01f60b877e7f1fc259e807b
Thomas Lisankie <link285@gmail.com>
2015-12-08 00:14:01 -0500

    Experimenting with tries

---
+++ src/RhymeDictionaryTrie.java
@@ -0,0 +1,29 @@
+
+public class RhymeDictionaryTrie implements Trie {
+
+	public boolean addWord(Word word) {
+		
+		/*
+		 * 
+		 * 
+		 * */
+		
+		return false;
+	}
+
+	public boolean removeWord(Word word) {
+		
+		return false;
+	}
+
+	public boolean hasWord(Word word) {
+		
+		return false;
+	}
+
+	public char[] getNextCharacters(String prefixString) {
+		
+		return null;
+	}
+
+}
