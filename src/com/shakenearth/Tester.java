ee427fd50c057317c70979de5b4864bbb0654383
Thomas Lisankie <link285@gmail.com>
2016-07-09 16:04:29 -0400

    -

--- src/com/shakenearth/Tester.java
+++ src/com/shakenearth/Tester.java
@@ -8,14 +8,12 @@ public class Tester {
 	
 	public static void main(String[] args){
 		
-		RhymeFinder finder = new RhymeFinder("/Users/thomas/Desktop/Dev/rap-writer/src/cmudict-0.7b_modified.txt");
-		List<Phoneme> nemes = finder.getTrie().getWord("ababa").getListOfPhonemes();
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Enter a word: ");
+		String word = reader.nextLine();
+		reader.close();
 		
-		for(int i = 0; i < nemes.size(); i++){
-			
-			System.out.println(nemes.get(i).getPhoneme());
-			
-		}
+		RhymeFinder finder = new RhymeFinder();
 		
 	}
 
